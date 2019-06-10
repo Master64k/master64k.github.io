dce624f8260afc1ffea19d60b5cfa2bf6a1f9720
Master64k <nicolas.nico@hotmail.com>
2019-06-09 22:05:12 -0300

    Initial commit.

---
+++ js/main.js
@@ -0,0 +1,39 @@
+//import * as faceapi from "./vendor/face-api";
+
+$(document).ready(function(){
+
+  faceapi.nets.ssdMobilenetv1.loadFromUri('models')
+
+
+  $('#file-input').on('change', async function() {
+
+    const uploadEl = document.getElementById('file-input').files[0];
+
+    const image = await faceapi.bufferToImage(uploadEl);
+
+    const detection = await faceapi.detectSingleFace(image);
+
+    if(detection !== undefined) {
+
+      UIkit.modal(document.getElementById('result-modal')).show();
+
+      const displaySize = { width: image.width, height: image.height }
+
+      $('#result-img').attr({'src': URL.createObjectURL(uploadEl)});
+
+      $('#result-img').css({width: image.width, height: image.height});
+
+      const canvas = document.getElementById('result-canvas');
+
+      faceapi.matchDimensions(canvas, displaySize)
+
+      faceapi.draw.drawDetections(canvas, faceapi.resizeResults(detection, displaySize))
+
+    } else alert('Nenhum rosto detectado');
+
+
+
+  })
+
+
+});
